a62498dab6cdefe275ae0037e7639e36dd9066a9
rob4everever <robbarton27@gmail.com>
2021-04-22 13:09:16 +0100

    added 'primaryColour' to the pageConfig as this will be used to match the navbar colour to the page colour which will vary depending on the page being viewed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,12 @@
 import Layout from '../components/layout';
 
 export default function Home() {
-  return (
-    <Layout pageConfig={pageConfig}>
-      <h1>Hello, World!</h1>
-    </Layout>
-  );
+  return <Layout pageConfig={pageConfig}></Layout>;
 }
 
 //passed to the metadata component to set some properties
 const pageConfig = {
   title: 'home',
   description: 'Portfolio website built by @robbatr0n',
+  primaryColour: '#ffffff',
 };
