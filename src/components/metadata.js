806d664fdd0482cdbd0d72b9830ac3f99ba01062
rob4everever <robbarton27@gmail.com>
2021-04-22 18:12:17 +0100

    slight change to the viewport meta tag to prevent page issues when zoomed out.

--- src/components/metadata.js
+++ src/components/metadata.js
@@ -16,7 +16,7 @@ export default function Metadata({ pageTitle, pageDescription }) {
     <Head>
       <meta charSet="UTF-8" />
       <title>{pageTitle} | robbatron</title>
-      <meta name="viewport" content="width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={pageDescription} />
       <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
